7ab05e8f1217bd131152f713d9e0881423aae1ce
eandr127 <ryan.larkin@uwaterloo.ca>
2019-12-15 17:16:16 -0500

    Add multilanguage header support

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 import withI18next from "../components/withI18next"
+import { Helmet } from "react-helmet"
 
 export default withI18next({ ns: "common" })(({ t }) => (
-  <div>{t("greeting")}</div>
+  <>
+    <Helmet
+      defer={false}
+      defaultTitle={t("title")}
+      titleTemplate={`%s | ${t("title")}`}
+    ></Helmet>
+    <div>{t("greeting")}</div>
+  </>
 ))
