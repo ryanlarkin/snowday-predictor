d9b8648e7351cc90d543e2916a59a8ca30fbb92b
Dmitry Tsarapkine <dimazonyoutubing@gmail.com>
2019-12-20 14:57:24 -0500

    Created Model & Prediction Functionality
    Created trained model. Also, allows for user to predict using this trained model.

--- data/Scripts/create_model.py
+++ data/Scripts/create_model.py
@@ -8,17 +8,23 @@ snowday_data = dataset['Snowday?']
 weather_data = dataset.drop(
     ['Snowday?', 'Snow on Grnd (cm)', 'Cool Deg Days (°C)', 'Heat Deg Days (°C)', 'Mean Temp (°C)'], axis=1)
 
+print(weather_data)
+
 # define the keras model
 model = Sequential()
-model.add(Dense(50, input_dim=5, activation='relu'))
+model.add(Dense(30, input_dim=5, activation='relu'))
+model.add(Dense(11, input_dim=5, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit the keras model on the dataset
-model.fit(weather_data, snowday_data, epochs=200, batch_size=3)
+model.fit(weather_data, snowday_data, epochs=200, batch_size=100)
 
 # evaluate the keras model
 _, accuracy = model.evaluate(weather_data, snowday_data)
 print('Accuracy: %.2f' % (accuracy*100))
+
+# save the keras model to file
+model.save('model.h5')
---
+++ data/Scripts/predict.py
@@ -0,0 +1,9 @@
+from keras.models import load_model
+from numpy import array
+
+
+def predict_snowday(max_temp, min_temp, total_rain, total_snow, total_precip):
+    model = load_model('model.h5')
+    prediction = model.predict(array([[max_temp, min_temp, total_rain, total_snow, total_precip]]))[0][0]
+
+    return prediction
