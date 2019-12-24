0a88628f8cf6be7f0df2009907540d9e24289177
eandr127 <ryan.larkin@uwaterloo.ca>
2019-12-24 15:38:09 -0500

    Make nicer loader

--- frontend/src/components/Loading/Loader.tsx
+++ frontend/src/components/Loading/Loader.tsx
@@ -1,4 +1,17 @@
 import React from "react"
-import { ProgressBar } from "react-bootstrap"
+import Loader from "react-loader-spinner"
+import styled from "styled-components"
 
-export default () => <ProgressBar animated now={100} />
+const StyledLoader = styled(Loader)`
+  display: flex;
+  justify-content: center;
+
+  svg {
+    width: 5rem !important;
+    height: 5rem !important;
+  }
+`
+
+export default () => (
+  <StyledLoader type="Bars" color="#C4C4C4" height={80} width={80} />
+)
