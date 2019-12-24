42e7754392ebcd48b9e63166016f5dd8509f9c09
eandr127 <ryan.larkin@uwaterloo.ca>
2019-12-24 14:05:54 -0500

    Centre input, tweak form feedback, fix error in tsconfig.json

--- frontend/src/components/UserInput/index.tsx
+++ frontend/src/components/UserInput/index.tsx
@@ -7,6 +7,7 @@ import { connect } from "react-redux"
 import { Form, Button, Col } from "react-bootstrap"
 import { Formik } from "formik"
 import { string, object } from "yup"
+import styled from "styled-components"
 
 const schema = object({
   postalCode: string()
@@ -24,46 +25,95 @@ const mapStateToProps = (props: GlobalState) => ({
   t: props.i18nReducer.t,
 })
 
+const StyledUserInput = styled.div`
+  input[name="postalCode"] {
+    flex-grow: 1;
+  }
+
+  .submitCol {
+    flex-grow: 0;
+  }
+
+  .inputLabel {
+    font-size: 1.2rem;
+    color: #ccdbdc !important;
+  }
+
+  .inputSubmit {
+    background-color: #56a3a6 !important;
+    border-color: #56a3a6 !important;
+    color: #ccdbdc !important;
+
+    :hover {
+      background-color: #60acb0 !important;
+      border-color: #60acb0 !important;
+    }
+  }
+
+  .form-row {
+    justify-content: center;
+  }
+`
+
 export default connect(mapStateToProps)(({ t }: ConnectedUserInput) => (
-  <Formik
-    validationSchema={schema}
-    onSubmit={console.log}
-    initialValues={{
-      postalCode: "",
-    }}
-  >
-    {({
-      handleSubmit,
-      handleChange,
-      handleBlur,
-      values,
-      touched,
-      isValid,
-      errors,
-    }) => (
-      <Form noValidate onSubmit={handleSubmit}>
-        <Form.Row>
-          <Form.Group as={Col} md="4" controlId="postalCodeValidation">
-            <Form.Label>{t("inputLabel")}</Form.Label>
-            <Form.Control
-              type="text"
-              name="postalCode"
-              placeholder={t("inputPlaceholder")}
-              value={values.postalCode}
-              onChange={handleChange}
-              isValid={values.postalCode && !errors.postalCode}
-              isInvalid={touched.postalCode && !!errors.postalCode}
-              onBlur={handleBlur}
-            />
-            <Form.Control.Feedback type="invalid">
-              {t("inputInvalid")}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button disabled={!isValid || !touched} type="submit">
-            {t("submitButton")}
-          </Button>
-        </Form.Row>
-      </Form>
-    )}
-  </Formik>
+  <StyledUserInput>
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        postalCode: "",
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        setTouched,
+        errors,
+      }) => (
+        <Form className="form" noValidate onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} md="5" controlId="postalCodeValidation">
+              <Form.Label className="inputLabel">{t("inputLabel")}</Form.Label>
+              <Form.Row>
+                <Col>
+                  <Form.Control
+                    type="text"
+                    name="postalCode"
+                    placeholder={t("inputPlaceholder")}
+                    value={values.postalCode}
+                    onChange={handleChange}
+                    isValid={touched.postalCode && !errors.postalCode}
+                    isInvalid={touched.postalCode && !!errors.postalCode}
+                    onBlur={handleBlur}
+                    onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                      // Give feedback when user presses enter
+                      if (e.key === "Enter") {
+                        setTouched({ postalCode: true })
+                      }
+                    }}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {t("inputInvalid")}
+                  </Form.Control.Feedback>
+                </Col>
+                <Col className="submitCol">
+                  <Button
+                    className="inputSubmit"
+                    disabled={!isValid || !values.postalCode}
+                    type="submit"
+                  >
+                    {t("submitButton")}
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      )}
+    </Formik>
+  </StyledUserInput>
 ))
