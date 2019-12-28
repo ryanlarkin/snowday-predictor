1dd2f20cf054276fa0accc491a96b66aae3a0c5b
Dmitry Tsarapkine <dimazonyoutubing@gmail.com>
2019-12-27 22:49:40 -0500

    Fixing Backend

---
+++ backend/src/graphql.js
@@ -0,0 +1,37 @@
+// graphql.js
+
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ 
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context
+  }),
+  introspection: true,
+  playground: true
+});
+
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
