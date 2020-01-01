539672325742ba8eec1141d79faaecaab83f8179
eandr127 <ryan.larkin@uwaterloo.ca>
2020-01-01 18:59:47 -0500

    Fix OutboundLink not recording properly

--- frontend/src/components/OutboundLink/index.tsx
+++ frontend/src/components/OutboundLink/index.tsx
@@ -2,31 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Button, ButtonProps } from "react-bootstrap"
 
-const createFunctionWithTimeout = (
-  callback: () => void,
-  opt_timeout = 1000
-) => {
-  let called = false
-  const raceCallback = () => {
-    if (!called) {
-      called = true
-      callback()
-    }
-  }
-  setTimeout(raceCallback, opt_timeout)
-  return raceCallback
-}
-
+/**
+ * Button that tracks click in Google Analytics
+ * @param props The properties to pass to the button
+ */
 const OutboundLink = (
   props: OutboundLinkProps & React.HTMLProps<HTMLButtonElement> & ButtonProps
 ) => (
   <Button
     {...(props as any)}
     onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      if (typeof props.onClick === `function`) {
+      if (typeof props.onClick === "function") {
         props.onClick(e)
       }
-      let redirect = true
+
+      var redirect = true
+
       if (
         e.button !== 0 ||
         e.altKey ||
@@ -37,16 +28,17 @@ const OutboundLink = (
       ) {
         redirect = false
       }
-      if (props.target && props.target.toLowerCase() !== `_self`) {
+
+      if (props.target && props.target.toLowerCase() !== "_self") {
         redirect = false
       }
-      if ((window as any).ga) {
-        ;(window as any).ga(`send`, `event`, {
-          eventCategory: `Outbound Link`,
-          eventAction: `click`,
-          eventLabel: props.href,
-          transport: redirect ? `beacon` : ``,
-          hitCallback: function() {
+
+      if ((window as any).gtag) {
+        ;(window as any).gtag("event", "click", {
+          event_category: "outbound",
+          event_label: props.href,
+          transport_type: redirect ? "beacon" : "",
+          event_callback: function event_callback() {
             if (redirect && props.href) {
               document.location.href = props.href
             }
@@ -70,56 +62,13 @@ OutboundLink.propTypes = {
 }
 
 /**
- * This allows the user to create custom events within their Gatsby projects.
- *
- * @param {import('gatsby-plugin-google-analytics').CustomEventArgs} args
- * @see https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#events
+ * Properties for OutboundLink component
  */
-function trackCustomEvent({
-  category,
-  action,
-  label,
-  value,
-  nonInteraction = true,
-  transport,
-  hitCallback,
-  callbackTimeout = 1000,
-}: CustomEventArgs): void {
-  if (typeof window !== `undefined` && (window as any).ga) {
-    const trackingEventOptions = {
-      eventCategory: category,
-      eventAction: action,
-      eventLabel: label,
-      eventValue: value,
-      nonInteraction: nonInteraction,
-      transport,
-      hitCallback: () => {},
-    }
-
-    if (hitCallback && typeof hitCallback === `function`) {
-      trackingEventOptions.hitCallback = createFunctionWithTimeout(
-        hitCallback as () => void,
-        callbackTimeout
-      )
-    }
-
-    ;(window as any).ga(`send`, `event`, trackingEventOptions)
-  }
-}
-
 interface OutboundLinkProps {
+  /**
+   * What to do when the button is clicked
+   */
   onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void
 }
 
-export interface CustomEventArgs {
-  category: string
-  action: string
-  label?: string
-  value?: string
-  nonInteraction?: boolean
-  transport?: "beacon" | "xhr" | "image"
-  hitCallback?: Function
-  callbackTimeout?: number
-}
-
-export { OutboundLink, trackCustomEvent }
+export { OutboundLink }
