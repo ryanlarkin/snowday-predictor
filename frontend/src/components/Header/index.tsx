606d2e44a2cca7bf4e79d65d2f260abc33b2cd1f
eandr127 <ryan.larkin@uwaterloo.ca>
2020-01-03 16:33:03 -0500

    Add meta tags for page

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet"
 import React from "react"
 import { connect } from "react-redux"
 import { GlobalState } from "../../types/types"
-import { TFunction } from "i18next"
+import { TFunction, i18n } from "i18next"
 
 /**
  * Type declaration for component props
@@ -12,6 +12,11 @@ type ConnectedHeader = {
    * Translation function
    */
   t: TFunction
+
+  /**
+   * Language information
+   */
+  i18n: i18n
 
   /**
    * Data recieved from static query
@@ -31,16 +36,28 @@ type ConnectedHeader = {
 
 const mapStateToProps = (props: GlobalState) => ({
   t: props.t,
+  i18n: props.i18n,
 })
 
 /**
  * Sets header information including latest Git commit, and page title
  */
-export default connect(mapStateToProps)(({ t, queryData }: ConnectedHeader) => (
-  <Helmet
-    defer={false}
-    defaultTitle={t("title")}
-    titleTemplate={`%s | ${t("title")}`}
-    meta={[{ name: "commit", content: queryData.gitCommit.hash }]}
-  />
-))
+export default connect(mapStateToProps)(
+  ({ t, i18n, queryData }: ConnectedHeader) => (
+    <Helmet
+      defer={false}
+      defaultTitle={t("title")}
+      titleTemplate={`%s | ${t("title")}`}
+      meta={[
+        { name: "commit", content: queryData.gitCommit.hash },
+        { name: "description", content: t("siteDescription") },
+        { name: "robots", content: "index, follow" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { httpEquiv: "Content-Type", content: "text/html; charset=utf-8" },
+      ]}
+    >
+      <link rel="canonical" href="https://www.snowdaypredictor.net" />
+      <html lang={i18n.language.substring(0, 2)} />
+    </Helmet>
+  )
+)
